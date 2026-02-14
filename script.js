7cfad249afe44e1265c35ead436be2a74dd40930
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-14 14:43:38 +0530

    Selected #et200, #et500, and #et2000 elements on DOMContentLoaded

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const et2000 = document.getElementById('et2000');
     const et500 = document.getElementById('et500');
+    const et200 = document.getElementById('et200');
 

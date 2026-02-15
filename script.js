8aa652d37980bc7d909c13ce4d8e9d1b35155009
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-15 14:49:04 +0530

    Called totalCash function to update total amount after each input

--- script.js
+++ script.js
@@ -40,5 +40,6 @@ cashInputs.forEach((input, index) => {
       const denominations = [2000, 500, 200, 100,50,20,10,5,2,1];
       const rowValue = cashInputs[index].value * denominations[index];
       cashTexts[index].textContent = rowValue.toFixed(0);
-
+      totalCash();
+    }
                 

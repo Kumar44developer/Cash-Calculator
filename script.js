d6cdf9bdbbe9b3467f3abfe50b614da584a77248
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-15 14:54:28 +0530

    Defined teens array for convertToWords function

--- script.js
+++ script.js
@@ -64,6 +64,7 @@ cashInputs.forEach((input, index) => {
 
    function convertToWords(number) {
       const units = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+      const teens = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
 
 
                           
